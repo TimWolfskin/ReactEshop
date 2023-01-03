63d3ab66324ae7ab1603f050bb9a8390a6495a31
TimWolfskin <timwolfskin013@gmail.com>
2023-01-03 01:18:11 -0800

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Home/Product/Product"
+import Products from "./pages/Home/Products/Products"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar";
+import "./app.scss"
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+//import { persistor, store } from "./redux/store";
+//import { Provider } from "react-redux";
+//import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+        <App />
   </React.StrictMode>
 );
-
